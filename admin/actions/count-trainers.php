ac25831fd9bf97429821c808bba3b3b724368942
leslie-23 <leslieajayi27@gmail.com>
2025-04-25 21:36:38 +0000

    stable build

---
+++ admin/actions/count-trainers.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername="localhost";
+$uname="new_user";
+$pass="new_password";
+$db="elitefit-23";
+
+$conn=mysqli_connect($servername,$uname,$pass,$db);
+
+if(!$conn){
+    die("Connection Failed");
+}
+
+$sql = "SELECT * FROM staffs WHERE designation='Trainer'";
+                $query = $conn->query($sql);
+
+                echo "$query->num_rows";
+?> 
---
+++ admin/income-count.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$servername="localhost";
+$uname="new_user";
+$pass="new_password";
+$db="elitefit-23";
+
+$conn=mysqli_connect($servername,$uname,$pass,$db);
+
+if(!$conn){
+    die("Connection Failed");
+}
+
+$sql = "SELECT SUM( amount) FROM members";
+        $amountsum = mysqli_query($conn, $sql) or die(mysqli_error($sql));
+        $row_amountsum = mysqli_fetch_assoc($amountsum);
+        $totalRows_amountsum = mysqli_num_rows($amountsum);
+        echo $row_amountsum['SUM( amount)'];
+?> 
